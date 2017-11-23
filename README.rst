1b33710ce12125392ef1bf8548ad673994a2e2fb
wojciech11 <wojciechbarczynski.wsb+1@gmail.com>
2017-11-23 01:04:48 +0100

    Init

---
+++ README.rst
@@ -0,0 +1,11 @@
+=======================
+01 First Python Program
+=======================
+
+::
+
+  python main.py
+
+::
+
+  flake8 main.py
---
+++ main.py
@@ -0,0 +1,8 @@
+def main(imie):
+    print("Hello World!")
+    print(imie)
+
+
+if __name__ == "__main__":
+    imie = "Wojtek"
+    main(imie)
